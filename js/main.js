1aed88db134c2167b413b4badd96b731ef8fe2d7
Nico <nbalvarezo@gmail.com>
2024-03-27 16:12:35 -0300

    hola, mundo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+function enContruccion() {
+  alert("Sitio en construcción");
+}
 
-function enContruccion(){
- alert("Sitio en construcción");
+function escribirEnPantalla(texto) {
+  let arr = texto.split("");
+  
+  i = 0;
+  
+  let intervalo = setInterval(function () {
+    if (i == arr.length - 1) {
+      document.getElementById("titulo").innerHTML += arr[i];
+      clearInterval(intervalo);
+    } else {
+      if (arr[i] == " ") {
+        document.getElementById("titulo").innerHTML += arr[i];
+        document.getElementById("titulo").innerHTML += arr[i + 1];
+        i += 2;
+      } else {
+        document.getElementById("titulo").innerHTML += arr[i];
+        i++;
+      }
+    }
+  }, 100);
 }
